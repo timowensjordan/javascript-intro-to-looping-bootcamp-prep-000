61383957a7c767512dbbdadc445b35463a5c2f7f
Tim Jordan <raeyejordan@yahoo.com>
2018-01-22 18:03:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function doWhileLoop(array){
     return Math.random()>=0.5;
   }
   do{
-    array.pop(array());
+    array.pop(array);
   }while (array.length > 0 && maybeTrue());
   return array;
 }
